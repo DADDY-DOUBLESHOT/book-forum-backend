fe323d1d4fa961191346204c3f7bf2d556486317
Pratham Sonawane <78498799+DADDY-DOUBLESHOT@users.noreply.github.com>
2023-04-26 23:24:45 +0530

    wishlist fixed

--- controllers/whishlistController.js
+++ controllers/whishlistController.js
@@ -5,74 +5,75 @@ const Book = require("../models/bookModel");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 exports.addWhishlist = catchAsyncErrors(async (req, res, next) => {
-    const bookId = req.body.bookId;
-    const userId = req.user.id;
+  const bookId = req.body.bookId;
+  const userId = req.user.id;
 
-    const book = await Book.findById(bookId);
+  const book = await Book.findById(bookId);
 
-    console.log(book)
+  console.log(book);
 
-    if (!book) {
-        return res.status(404).json({ error: 'Book not found' });
-    }
+  if (!book) {
+    return res.status(404).json({ error: "Book not found" });
+  }
 
-    let wishlist = await Wishlist.findOne({ user: userId });
+  let wishlist = await Wishlist.findOne({ user: userId });
 
-    if (!wishlist) {
-        console.log("inside if")
-        // If wishlist not found for user, create a new wishlist with the book
-        wishlist = await Wishlist.create({ user: userId, books: { book: book } });
-        await wishlist.save();
-        return res.status(201).json(wishlist);
-    } else {
-        // If wishlist found for user, add book to wishlist
-        console.log("inside else")
-        wishlist.books.push({ book });
-        await wishlist.save();
-        return res.json(wishlist);
-    }
-})
+  if (!wishlist) {
+    console.log("inside if");
+    // If wishlist not found for user, create a new wishlist with the book
+    wishlist = await Wishlist.create({ user: userId, books: { book: book } });
+    await wishlist.save();
+    return res.status(201).json(wishlist);
+  } else {
+    // If wishlist found for user, add book to wishlist
+    console.log("inside else");
+    wishlist.books.push({ book });
+    await wishlist.save();
+    return res.json(wishlist);
+  }
+});
 
 exports.removeBook = catchAsyncErrors(async (req, res, next) => {
-    const id = req.body.id;
-    const userId = req.user.id;
+  const id = req.body.id;
+  const userId = req.user.id;
 
-    const wishlist = await Wishlist.findOneAndUpdate(
-        { user: userId },
-        { $pull: { books: { _id: id } } },
-        { new: true }
-    );
+  const wishlist = await Wishlist.findOneAndUpdate({ user: userId }, { $pull: { books: { _id: id } } }, { new: true });
 
-    if (!wishlist) {
-        return res.status(404).json({ error: 'Wishlist not found' });
-    }
+  if (!wishlist) {
+    return res.status(404).json({ error: "Wishlist not found" });
+  }
 
-    return res.json(wishlist);
+  wishlist.remove();
+
+  return res.status(200).json({
+    success: true,
+    message: "Book removed..",
+  });
 });
 
 exports.getWhishlist = catchAsyncErrors(async (req, res) => {
-    const userId = req.user.id;
+  const userId = req.user.id;
 
-    const wishlist = await Wishlist.findOne({ user: userId });
+  const wishlist = await Wishlist.findOne({ user: userId });
 
-    if (!wishlist) {
-        return res.status(404).json({ error: 'Wishlist not found' });
-    }
+  if (!wishlist) {
+    return res.status(404).json({ error: "Wishlist not found" });
+  }
 
-    return res.json(wishlist.books);
-})
+  return res.json(wishlist.books);
+});
 
 exports.clearWishlist = catchAsyncErrors(async (req, res, next) => {
-    const userId = req.user.id;
+  const userId = req.user.id;
 
-    const wishlist = await Wishlist.findOne({ user: userId });
+  const wishlist = await Wishlist.findOne({ user: userId });
 
-    if (!wishlist) {
-        return res.status(404).json({ error: "Wishlist not found" });
-    }
+  if (!wishlist) {
+    return res.status(404).json({ error: "Wishlist not found" });
+  }
 
-    wishlist.books = [];
-    await wishlist.save();
+  wishlist.books = [];
+  await wishlist.save();
 
-    return res.json(wishlist);
+  return res.json(wishlist);
 });
